4d03fc8c6db3af817e8ec5db631e15d9e8563e10
JerryShen <hansay99@gmail.com>
2012-01-29 19:36:33 +0800

    update load path

--- config/setup_load_paths.rb
+++ config/setup_load_paths.rb
@@ -5,7 +5,7 @@ if ENV['MY_RUBY_HOME'] && ENV['MY_RUBY_HOME'].include?('rvm')
     $LOAD_PATH.unshift rvm_lib_path
     require 'rvm'
     # RVM.use_from_path! File.dirname(File.dirname(__FILE__))
-    RVM.gemset_use! 'red2012'
+    RVM.use_from_path! File.dirname(File.dirname(__FILE__))
   rescue LoadError
     # RVM is unavailable at this point.
     raise "RVM ruby lib is currently unavailable."
